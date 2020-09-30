6f304c994b909c229b27335717f970f50e797c90
joshpetit <joshuapetitma@yahoo.com>
2020-09-30 07:25:33 -0400

    added and formatted Verse class

---
+++ lib/model/Verse.dart
@@ -0,0 +1,8 @@
+class Verse {
+  final String reference;
+  final String book;
+  final int chapter;
+  final int verse_number;
+
+  Verse._(this.reference, this.book, this.chapter, this.verse_number);
+}
