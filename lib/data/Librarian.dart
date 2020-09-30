1707b754e8740461ad39e0861d736a9be9682d73
joshpetit <joshuapetitma@yahoo.com>
2020-09-30 08:31:08 -0400

    feat: basic Librarian implementation

--- lib/data/Librarian.dart
+++ lib/data/Librarian.dart
@@ -2,9 +2,19 @@ import 'package:reference_parser/data/BibleData.dart';
 
 class Librarian {
   static int findBook(String book) {
-    return BibleData.books[book];
+    book = book.toLowerCase();
+    var val = BibleData.books[book];
+    if (val != null) {
+      return val;
+    }
+    val = BibleData.osis_books[book];
+    if (val != null) {
+      return val;
+    }
+    return BibleData.books[book.toLowerCase()];
   }
   static bool checkBook(String book) {
-    return BibleData.books.containsKey(book);
+    return BibleData.books.containsKey(book) ||
+        BibleData.osis_books.containsKey(book);
   }
 }
