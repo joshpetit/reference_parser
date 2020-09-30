1645961e087751561c0ad3ccf10548fe92c70d11
joshpetit <joshuapetitma@yahoo.com>
2020-09-30 09:21:00 -0400

    feat: checkBook validates despite the String case

--- lib/data/Librarian.dart
+++ lib/data/Librarian.dart
@@ -1,6 +1,7 @@
 import 'package:reference_parser/data/BibleData.dart';
 
 class Librarian {
+  ///Returns the integer of the book, checks for variant spellings also
   static int findBook(String book) {
     book = book.toLowerCase();
     var val = BibleData.books[book];
@@ -12,9 +13,9 @@ class Librarian {
       return val;
     }
    return BibleData.variants[book];
-
   }
   static bool checkBook(String book) {
+    book = book.toLowerCase();
     return BibleData.books.containsKey(book) ||
         BibleData.osis_books.containsKey(book);
   }
