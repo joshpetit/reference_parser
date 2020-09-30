7a5e10a60e68eb2a7a7d2332bc9ea6a5bd58571d
joshpetit <joshuapetitma@yahoo.com>
2020-09-30 06:44:20 -0400

    made basic framework for Reference

---
+++ lib/model/Reference.dart
@@ -0,0 +1,26 @@
+class Reference {
+  String reference;
+  final String book;
+  final int chapter;
+  final int start_verse;
+  final int end_verse;
+  bool is_valid;
+
+  Reference(this.book, {this.chapter, this.start_verse, this.end_verse}) {
+    reference = book;
+    if (chapter != null) {
+      reference += ' ${chapter}';
+      if (start_verse != null) {
+        reference += ':${start_verse}';
+        if (end_verse != null) {
+          reference += '-$end_verse';
+        }
+      }
+    }
+    is_valid = validate();
+  }
+
+  bool validate() {
+    return true;
+  }
+}
