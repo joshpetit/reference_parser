903c284b411ec664755f0f28ab844b6f1987c0c5
joshu <joshuapetitma@yahoo.com>
2020-10-06 07:01:57 -0400

    refactor: initialized final variables before constructor

--- lib/model/Reference.dart
+++ lib/model/Reference.dart
@@ -1,26 +1,22 @@
 import 'package:reference_parser/data/Librarian.dart';
 
 class Reference {
-  String reference;
+  final String reference;
   final String book;
   final int chapter;
   final int start_verse;
   final int end_verse;
   bool is_valid;
 
-  Reference(this.book, [this.chapter, this.start_verse, this.end_verse]) {
-    reference = book;
-    if (chapter != null) {
-      reference += ' ${chapter}';
-      if (start_verse != null) {
-        reference += ':${start_verse}';
-        if (end_verse != null) {
-          reference += '-$end_verse';
-        }
-      }
-    }
+  Reference(book, [chapter, start_verse, end_verse])
+      : book = book,
+        chapter = chapter,
+        start_verse = start_verse,
+        end_verse = end_verse,
+        reference = Librarian.createReference(book, chapter, start_verse, end_verse) {
     is_valid = _validate();
   }
+
 
   bool _validate() {
     return Librarian.verifyVerse(book, chapter, start_verse);
