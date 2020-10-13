5949a823723cf48171852b8462904feebcc023c9
joshu <joshuapetitma@yahoo.com>
2020-10-13 06:31:06 -0400

    added createReference for direct creation

--- lib/src/parsers/parser.dart
+++ lib/src/parsers/parser.dart
@@ -1,8 +1,6 @@
 import 'package:reference_parser/src/data/Librarian.dart';
 import 'package:reference_parser/src/model/Reference.dart';
 
-
-
 var exp = RegExp(r'([1-3]?[^\s]+[A-Za-z ]+[^\d ]?) ?(\d+)?:?(\d+)?-?(\d+)?');
 
 Map<String, String> _parseString(String reference) {
@@ -23,8 +21,9 @@ Reference parseReference(String stringReference) {
   var pr = _parseString(stringReference);
   if (pr == null) return Reference('');
   var bookNumber = Librarian.findBook(pr['book']);
-  var book =
-  bookNumber != null ? Librarian.getBookNames(bookNumber)['name'] : pr['book'];
+  var book = bookNumber != null
+      ? Librarian.getBookNames(bookNumber)['name']
+      : pr['book'];
   var reference = Reference(
       book ?? pr['book'],
       pr['chapter'] == null ? null : int.parse(pr['chapter']),
@@ -32,4 +31,12 @@ Reference parseReference(String stringReference) {
       pr['endVerse'] == null ? null : int.parse(pr['endVerse']));
 
   return reference;
+}
+
+Reference createReference(String book,
+    [int chapter, int startVerse, int endVerse]) {
+  var name = Librarian.getBookNames(book);
+  book = name != null ? name['name'] : book;
+  return Reference(
+      book, chapter = chapter, startVerse = startVerse, endVerse = endVerse);
 }
