d225cffcd7b53aa1d48427fa680fcf3c75034ad0
joshu <joshuapetitma@yahoo.com>
2020-10-20 06:17:00 -0400

    added tests for Reference.referenceType

--- test/reference_test.dart
+++ test/reference_test.dart
@@ -1,5 +1,6 @@
 import 'package:reference_parser/src/model/Reference.dart';
 import 'package:reference_parser/src/model/Verse.dart';
+import 'package:reference_parser/src/util/VerseEnum.dart';
 import 'package:test/test.dart';
 
 void main() {
@@ -7,12 +8,14 @@ void main() {
     var ref = Reference('James');
     expect(ref.book, equals('James'));
     expect(ref.reference, equals('James'));
+    expect(ref.referenceType, equals(ReferenceType.BOOK));
   });
   test('Creation of book and chapter reference', () {
     var ref = Reference('James', 5);
     expect(ref.reference, equals('James 5'));
     expect(ref.book, equals('James'));
     expect(ref.chapter, equals(5));
+    expect(ref.referenceType, equals(ReferenceType.CHAPTER));
   });
   test('Creation of verse reference', () {
     var ref = Reference('James', 5, 2);
@@ -20,14 +23,16 @@ void main() {
     expect(ref.book, equals('James'));
     expect(ref.chapter, equals(5));
     expect(ref.startVerseNumber, equals(2));
+    expect(ref.referenceType, equals(ReferenceType.VERSE));
   });
-  test('Creation of verse reference', () {
+  test('Creation of range of verses reference', () {
     var ref = Reference('James', 5, 2, 3);
     expect(ref.reference, equals('James 5:2-3'));
     expect(ref.book, equals('James'));
     expect(ref.chapter, equals(5));
     expect(ref.startVerseNumber, equals(2));
     expect(ref.endVerseNumber, equals(3));
+    expect(ref.referenceType, equals(ReferenceType.RANGE));
   });
   test('Verification works correctly', () {
     var ref = Reference('James', 2, 4);
@@ -41,14 +46,13 @@ void main() {
   });
   test('Book numbers correctly initialized', () {
     var ref = Reference('Genesis');
-    expect(ref.bookNumber, equals(1), reason: 'Genesis is the 1st book_number');
+    expect(ref.bookNumber, equals(1), reason: 'Genesis is the 1st book');
 
     ref = Reference('John');
-    expect(ref.bookNumber, equals(43), reason: 'John is the 43rd book_number');
+    expect(ref.bookNumber, equals(43), reason: 'John is the 43rd book');
 
     ref = Reference('Revelation');
-    expect(ref.bookNumber, equals(66),
-        reason: 'Revelation is the 66th book_number');
+    expect(ref.bookNumber, equals(66), reason: 'Revelation is the 66th book');
 
     ref = Reference('Joe');
     expect(ref.bookNumber, equals(null),
