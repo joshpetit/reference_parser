26606f1476def590bd7c27422b7d6d1fc7249bd0
joshu <joshuapetitma@yahoo.com>
2020-10-22 07:15:19 -0400

    renames constructor variables to allow if null operators

--- lib/src/model/Reference.dart
+++ lib/src/model/Reference.dart
@@ -15,32 +15,26 @@ class Reference {
   final ReferenceType referenceType;
   final bool isValid;
 
-  Reference(book, [chapter, startVerseNumber, endVerseNumber])
+  Reference(book, [chp, svn, evn])
       : book = book,
-        chapter = chapter != null ? chapter : 1,
-        startVerseNumber = startVerseNumber != null ? startVerseNumber : 1,
-        startVerse = startVerseNumber != null
-            ? Verse(book, chapter, startVerseNumber)
-            : chapter != null
-                ? Verse(book, chapter, 1)
+        chapter = chp ?? 1,
+        startVerseNumber = svn ?? 1,
+        startVerse = svn != null
+            ? Verse(book, chp, svn)
+            : chp != null
+                ? Verse(book, chp, 1)
                 : Verse(book, 1, 1),
-        endVerseNumber = endVerseNumber != null
-            ? endVerseNumber
-            : startVerseNumber != null
-                ? startVerseNumber
-                : Librarian.getLastVerseNumber(book, chapter),
-        endVerse = endVerseNumber != null
-            ? Verse(book, chapter, endVerseNumber)
-            : startVerseNumber != null
-                ? Verse(book, chapter, startVerseNumber)
-                : Librarian.getLastVerse(book, chapter),
+        endVerseNumber = evn ?? svn ?? Librarian.getLastVerseNumber(book, chp),
+        endVerse = evn != null
+            ? Verse(book, chp, evn)
+            : svn != null
+                ? Verse(book, chp, svn)
+                : Librarian.getLastVerse(book, chp),
         bookNumber = Librarian.findBook(book),
-        reference = Librarian.createReferenceString(
-            book, chapter, startVerseNumber, endVerseNumber),
-        referenceType = Librarian.identifyReferenceType(
-            book, chapter, startVerseNumber, endVerseNumber),
-        isValid = Librarian.verifyVerse(book, chapter, startVerseNumber) &&
-            Librarian.verifyVerse(book, chapter, endVerseNumber);
+        reference = Librarian.createReferenceString(book, chp, svn, evn),
+        referenceType = Librarian.identifyReferenceType(book, chp, svn, evn),
+        isValid = Librarian.verifyVerse(book, chp, svn) &&
+            Librarian.verifyVerse(book, chp, evn);
   @override
   String toString() {
     return reference;
