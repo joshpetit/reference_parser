751242bd66f39a87ec75c4490620cfbc1eebe40a
joshu <joshuapetitma@yahoo.com>
2020-10-27 07:11:14 -0400

    adds documentation to Reference object fields

--- lib/src/model/Reference.dart
+++ lib/src/model/Reference.dart
@@ -4,16 +4,47 @@ import 'package:reference_parser/src/model/BibleReference.dart';
 
 import 'package:reference_parser/src/util/VerseEnum.dart';
 
+/// A general BibleReference, can contain all [ReferenceType]
 class Reference extends BibleReference {
   @override
   final String reference;
+
+  /// The chapter number in this reference.
+  ///
+  /// Defaults to 1.
   final int chapter;
+
+  /// The first verse number found in this reference.
+  ///
+  /// Defaults to 1.
   final int startVerseNumber;
+
+  /// The [Verse] object representing the first verse in the reference.
+  ///
+  /// Returns the verse object of the first verse in the chapter if
+  /// [startVerse] is not specified.
+  /// Returns the verse object of the first verse in the book if
+  /// [chapter] is not specified.
+  /// Returns [null] if neither are specified.
   final Verse startVerse;
+
+  /// The last verse number found in this reference.
   final int endVerseNumber;
+
+  /// The [Verse] object representing the last verse in the reference.
+  ///
+  /// Returns the verse object of the last verse in the chapter if
+  /// [endVerse] is not specified.
+  /// Returns the verse object of the last verse in the book if
+  /// [chapter] is not specified.
+  /// Returns [null] if neither are specified.
   final Verse endVerse;
+
+  /// The type of reference. Either VERSE, RANGE, CHAPTER, or BOOK
   @override
   final ReferenceType referenceType;
+
+  /// Whether the reference is within the bible.
   @override
   final bool isValid;
 
