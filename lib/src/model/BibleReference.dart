08140e89b8b31cf202bceab6e72fbbf00277019f
joshu <joshuapetitma@yahoo.com>
2020-10-27 06:05:03 -0400

    adds osis abbr and short book names getters

--- lib/src/model/BibleReference.dart
+++ lib/src/model/BibleReference.dart
@@ -4,6 +4,7 @@ import 'package:reference_parser/src/data/Librarian.dart';
 abstract class BibleReference {
   final String reference;
   final String book;
+  final Map<String, String> _bookNames;
   final int bookNumber;
   final ReferenceType referenceType;
   final bool isValid;
@@ -13,9 +14,15 @@ abstract class BibleReference {
         reference = Librarian.createReferenceString(book),
         bookNumber = Librarian.findBook(book),
         referenceType = Librarian.identifyReferenceType(book),
+        _bookNames = Librarian.getBookNames(book),
         isValid = Librarian.verifyVerse(book);
 
+  @override
   String toString() {
     return reference;
   }
+
+  String get osis => _bookNames['osis'];
+  String get abbr => _bookNames['abbr'];
+  String get short => _bookNames['short'];
 }
