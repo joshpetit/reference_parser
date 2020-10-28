4b8e2a73ffaa85b8200ff854514eaf0990c1cf5f
joshu <joshuapetitma@yahoo.com>
2020-10-28 06:20:30 -0400

    adds documentation to Verse BibleReference

--- lib/src/model/Verse.dart
+++ lib/src/model/Verse.dart
@@ -2,13 +2,23 @@ import 'package:reference_parser/src/util/VerseEnum.dart';
 import 'package:reference_parser/src/data/Librarian.dart';
 import 'package:reference_parser/src/model/BibleReference.dart';
 
+/// A bible reference that contains the book, chapter, and a single verse number
 class Verse extends BibleReference {
+  /// The book, chapter, and verse number
   @override
   final String reference;
+
+  /// The chapter number this verse is within
   final int chapter;
+
+  /// The verse number this verse refers to within a chapter
   final int verseNumber;
+
+  /// [ReferenceType.VERSE]
   @override
   ReferenceType referenceType;
+
+  /// Whether this verse is found within the bible
   @override
   final bool isValid;
   Verse(book, chapter, verseNumber)
