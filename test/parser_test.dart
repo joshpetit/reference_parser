87b3bf21201361391f7355eaff79bcdb9156c477
joshu <joshuapetitma@yahoo.com>
2020-11-02 05:58:46 -0500

    adds tests for parsing first reference in string

--- test/parser_test.dart
+++ test/parser_test.dart
@@ -15,10 +15,6 @@ void main() {
     expect(ref.reference, equals('1 John 3:16'));
     expect(ref.startVerseNumber, equals(16));
 
-    ref = parseReference('Joe 2:4');
-    expect(ref.book, equals('Joe'));
-    expect(ref.isValid, false);
-
     ref = parseReference('Jn 2:4');
     expect(ref.book, equals('John'));
     expect(ref.book, 'John');
@@ -29,6 +25,27 @@ void main() {
     ref = parseReference('');
     expect(ref.book, '');
     expect(ref.isValid, false);
+
+    ref = parseReference('I love John 4:5-10');
+    expect(ref.book, equals('John'));
+    expect(ref.chapter, equals(4));
+    expect(ref.startVerseNumber, equals(5));
+    expect(ref.endVerseNumber, equals(10));
+
+    ref = parseReference('This is going to parse Isaiah');
+    expect(ref.book, equals('Isaiah'), reason: '\'is\' is parsed first');
+
+    ref = parseReference('Only jam should be parsed');
+    print(ref.reference);
+    expect(ref.book, equals('James'));
+
+    // I guess the Gospel of Joe does exist!
+    //
+    // TODO: Add paratext abbreviations to parsed books, filter out
+    // paratexts that are found in OSIS references.
+    //ref = parseReference('Joe 4:5-10');
+    //expect(ref.book, equals(''));
+    //expect(ref.isValid, equals(false));
   });
 
   test('Creation of reference from createReference', () {
