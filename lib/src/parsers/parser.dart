1e7db66ffdc4a23e081d03cb27b464e749e3e5d3
joshu <joshuapetitma@yahoo.com>
2020-11-02 05:37:18 -0500

    improves regex

--- lib/src/parsers/parser.dart
+++ lib/src/parsers/parser.dart
@@ -1,7 +1,8 @@
 import 'package:reference_parser/src/data/Librarian.dart';
+import 'package:reference_parser/src/data/BibleData.dart';
 import 'package:reference_parser/src/model/Reference.dart';
 
-var _exp = RegExp(r'([1-3]?[^\s]+[A-Za-z ]+[^\d ]?) ?(\d+)?:?(\d+)?-?(\d+)?');
+final _exp = _createFullRegex();
 
 Map<String, String> _parseString(String reference) {
   var match = _exp.firstMatch(reference);
@@ -9,19 +10,20 @@ Map<String, String> _parseString(String reference) {
   var thing = match.groups([0, 1, 2, 3, 4]);
   return {
     'reference': thing[0],
-    'book': thing[1].trim(),
+    'book': thing[1],
     'chapter': thing[2],
     'startVerse': thing[3],
     'endVerse': thing[4],
   };
 }
 
-/// Create a reference by parsing a string
+/// Finds the first reference from within a string.
 ///
-/// Currently only functions with simple references
+///
 /// ```dart
-///parseReference('Gen 2:4');
+/// parseReference('I love James 4:5 and Matthew 2:4');
 ///```
+/// Returns a reference object of James :45
 Reference parseReference(String stringReference) {
   var pr = _parseString(stringReference);
   if (pr == null) return Reference('');
@@ -44,4 +46,13 @@ Reference createReference(String book,
   book = Librarian.getBookNames(book)['name'] ?? book;
   return Reference(
       book, chapter = chapter, startVerse = startVerse, endVerse = endVerse);
+}
+
+RegExp _createFullRegex() {
+  var books = BibleData.bookNames.expand((i) => i).toList();
+  books.addAll(BibleData.variants.keys);
+  var regBooks = books.join('\\b|\\b');
+  var expression = '(\\b$regBooks\\b) ?(\\d+)?:?(\\d+)?-?(\\d+)?';
+  var exp = RegExp(expression, caseSensitive: false);
+  return exp;
 }
