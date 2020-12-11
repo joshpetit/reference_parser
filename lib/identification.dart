26760752531dc45cd9542aa69575c97413c08ea4
joshu <joshuapetitma@yahoo.com>
2020-12-11 06:43:23 -0500

    adds doc comment to identification library

--- lib/identification.dart
+++ lib/identification.dart
@@ -1,4 +1,9 @@
 /// Queries biblehub to identify the reference to strings
-
+///
+/// ```
+/// identifyReference("Come to me all ye who").then((x) => {
+///        print(x),
+///      });
+/// ```
 export 'src/parsers/identification.dart';
 export 'src/model/ReferenceQuery.dart';
