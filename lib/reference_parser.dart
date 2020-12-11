5599c3de36ca83634f814c6255a24b6fda8f3bc9
joshu <joshuapetitma@yahoo.com>
2020-12-10 20:54:14 -0500

    adds verse identification to exports

--- lib/reference_parser.dart
+++ lib/reference_parser.dart
@@ -1,5 +1,6 @@
 export 'src/model/BibleReference.dart';
 export 'src/parsers/parser.dart';
+export 'src/parsers/identification.dart';
 export 'src/model/Reference.dart';
 export 'src/model/Verse.dart';
 export 'src/util/VerseEnum.dart';
