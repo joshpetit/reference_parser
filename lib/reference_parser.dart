76d081dd13c29ce0c82949273544b89fa54a47be
joshu <joshuapetitma@yahoo.com>
2020-12-11 06:24:07 -0500

    move reference identification to its own library

--- lib/reference_parser.dart
+++ lib/reference_parser.dart
@@ -1,6 +1,5 @@
 export 'src/model/BibleReference.dart';
 export 'src/parsers/parser.dart';
-export 'src/parsers/identification.dart';
 export 'src/model/Reference.dart';
 export 'src/model/Verse.dart';
 export 'src/util/VerseEnum.dart';
