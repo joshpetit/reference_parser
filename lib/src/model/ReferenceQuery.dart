645959b499f45d1f85422f8df2e8aa5db0ec1285
joshu <joshuapetitma@yahoo.com>
2020-12-11 06:54:23 -0500

    adds docs to ReferenceQuery

--- lib/src/model/ReferenceQuery.dart
+++ lib/src/model/ReferenceQuery.dart
@@ -1,5 +1,17 @@
 import 'package:reference_parser/src/model/Reference.dart';
 
+/// A query object returned by the [identifyReference()] function.
+///
+/// The [preview] field contains a shortened portion of the verse.
+/// ```
+/// var refs = await identifyReference("This is what");
+/// var ref = refs[0];
+/// print(ref.preview) // "This is what the LORD says: "Heaven is my throne, and the earth is my footstool. ..."
+/// print(ref.query) // "This is what"
+/// print(ref.reference.reference) // "Isaiah 66:1"
+/// ```
+///
+/// *Note*, the [reference] field will be a [Reference] object, not a String.
 class ReferenceQuery {
   final String query;
   final Reference reference;
