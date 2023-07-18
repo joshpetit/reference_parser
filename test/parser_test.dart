d85fde338a85ea66a56a174fd900c3909b4bd231
Mark Skelton <mdskelton99@gmail.com>
2023-07-18 08:23:15 -0500

    Add another test

--- test/parser_test.dart
+++ test/parser_test.dart
@@ -101,6 +101,9 @@ void main() {
   test('Allow parsing numbered books without spaces', () {
     var ref = parseReference('1corinthians 1:23');
     expect(ref.reference, equals('1 Corinthians 1:23'));
+
+    ref = parseReference('1tm 2:1-3');
+    expect(ref.reference, equals('1 Timothy 2:1-3'));
   });
 
   test('Parsing all references', () {
